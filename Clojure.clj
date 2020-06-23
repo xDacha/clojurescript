2d1cf56da1f86938f2565d45ba20eb2cd12a3d4a
xDacha <46727989+xDacha@users.noreply.github.com>
2020-06-22 22:47:48 -0600

    Create Clojure.clj

---
+++ Clojure.clj
@@ -0,0 +1,8 @@
+(comment operacion de comparacion)
+(= (- 25 5) 20) 
+
+(comment operacion Basica de suma y resta) 
+(+ (- 8 4) 5)
+
+(comment ejemplo visto en clase compara la operacion con el resultado)
+(=(- (/ (+ 50(* 25 3)) 5) 4) 21)
